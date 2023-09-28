5a608d3fb93e7de8265e010862d7551088dbdf56
braaabdelmoaty3 <braaabdelmoaty3@gmail.com>
2023-09-28 15:42:04 -0700

    1

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -7,11 +7,16 @@
  */
 
 
-void _puts_recursion(char *s){
+void _puts_recursion(char *s)
+{
+	if (*s)
+	{
+		_putchar(*s);
+		_puts_recursion(s + 1);
+	}
 
-    printf("Puts with recursion");
+	else
+		_putchar('\n');
 }
 
-int main(){
-    _puts_recursion("Puts with recursion");
-}
+
